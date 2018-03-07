6e7e2a4ca5c7223f60dc7649473500bc9a91fba7
Jerem <jeremy.miara@devinci.fr>
2018-03-07 19:14:16 +0100

    First tests in the bitcoin folder

--- bitcoin/__tests__/convert.js
+++ bitcoin/__tests__/convert.js
@@ -4,18 +4,18 @@ const convert = require('..');
 const Big = require('big.js');
 
 test('should default to returning a Number', () => {
-  //convert(2, 'BTC', 'BTC');
-  throw new Error('test not yet defined... write your test here');
+  expect(convert(2, 'BTC', 'BTC')).toBe(2);
+  //throw new Error('test not yet defined... write your test here');
 });
 
 test('should return a Number', () => {
-  //convert(2, 'BTC', 'BTC', 'Number');
-  throw new Error('test not yet defined... write your test here');
+  expect(typeof convert(2, 'BTC', 'BTC', 'Number')).toBe('number');
+  //throw new Error('test not yet defined... write your test here');
 });
 
 test('should return a Big number', () => {
-  //convert(2, 'BTC', 'BTC', 'Big');
-  throw new Error('test not yet defined... write your test here');
+  expect(typeof convert(2, 'BTC', 'BTC', 'Big')).toBe('big');
+  //throw new Error('test not yet defined... write your test here');
 });
 
 test('should return a String', () => {
